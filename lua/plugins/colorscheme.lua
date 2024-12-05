3fd7736672637b7fe30138b8020a8170d031ba5c
Daniel Van <danielv@vanfamily.com>
2024-12-05 23:55:24 +0100

    Add initial config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  -- add gruvbox
+  {
+    'ellisonleao/gruvbox.nvim',
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'gruvbox'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
