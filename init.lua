f333c700a47a90a38e5c3dc245e335e33ec85345
Daniel Van <daniel@vanfamily.com>
2024-12-10 23:16:11 +0100

    add flash for faster find

--- init.lua
+++ init.lua
@@ -101,6 +101,19 @@ require 'keymaps'
 
 require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   -- This is often very useful to both group configuration, as well as handle
