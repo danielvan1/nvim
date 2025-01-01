e2286b6dee04d5afebe0efdcba37f17e033123b9
Daniel Van <daniel@vanfamily.com>
2025-01-01 23:04:10 +0100

    remove highlight for git change

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -16,7 +16,7 @@ return {
 
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
       numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
       current_line_blame = true,
 
