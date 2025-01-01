dc8b72669c6a701f21321223759c98cd83ef40ef
Daniel Van <daniel@vanfamily.com>
2025-01-01 23:03:29 +0100

    add back search for neovim config files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -165,5 +165,9 @@ return {
         prompt_title = 'Live Grep in Open Files',
       }
     end, { desc = '[S]earch [/] in Open Files' })
+
+    vim.keymap.set('n', '<leader>sn', function()
+      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+    end, { desc = '[S]earch [N]eovim files' })
   end,
 }
